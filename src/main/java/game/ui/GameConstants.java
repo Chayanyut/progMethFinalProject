81f50b2344e461854644926aefde362490766b76
itzshen <shensmartboyz@gmail.com>
2026-05-09 11:12:29 +0700

    add WORLD_WIDTH and WORLD_HEIGHT

--- src/main/java/game/ui/GameConstants.java
+++ src/main/java/game/ui/GameConstants.java
@@ -9,4 +9,6 @@ public final class GameConstants {
     private GameConstants() {}
 
     public static final double TILE_SIZE = 50.0;
+    public static final double WORLD_WIDTH  = 2000.0;
+    public static final double WORLD_HEIGHT = 1600.0;
 }
